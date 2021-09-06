c21064469cdcac5cbdf9722226d94686add2479d
Donald Frederick <donald@neurofoo.com>
2021-09-05 20:20:22 -0400

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const App = () => {
     return (
         <div>
-            <h1>hello world</h1>
+            <h1 className="bg-blue-500">hello world</h1>
         </div>
     );
 };
