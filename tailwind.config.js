f04d939d2298ccb84cedb4d3d9f8d7fefaaf0255
Donald Frederick <donald@neurofoo.com>
2021-09-05 20:37:23 -0400

    add given css and data values

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
     purge: {mode: "all", content: ["./src/**/*.{js,jsx,ts,tsx}"]},
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                "light-gray": "#ededed",
+                "medium-gray": "#777777",
+                "dark-gray": "#444444",
+                "dark-blue": "#0011bb",
+                "dark-purple": "#6600cc"
+            }
+        }
     },
     variants: {
         extend: {}
